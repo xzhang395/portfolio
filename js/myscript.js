af2b56d78aaa4ef2320f75d2a1e02b7e038e6c52
xzhang395 <xuetingzhang313@gmail.com>
2018-09-04 01:19:49 -0400

    added present

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    var vid = document.getElementById("myVideo");
+    vid.playbackRate = 2;
     var isMobile = false; //initiate as false
     // device detection
     if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
